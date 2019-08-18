2ff3535d8e18cf3eb24f9d9e66e6adf1660f5351
chakal05 <chakirrasta@gmail.com>
2019-08-18 09:41:05 +0200

    first commit

---
+++ php/index.php
@@ -0,0 +1,254 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Grenze&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/index.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <header>
+    <?php include('../php/header.php'); ?>
+    </header>
+  
+
+    <div class='main'>
+    <div class='picPub'>
+        <div class="text">
+        <h1> CJED</h1>
+        <p class="pOne"> <span class="sOne"><i>Inspire.</i></span>  <span class="sTwo"><i>Motive.</i></span>  <span class="sThree"><i>Guide.</i></span> </p>
+        <button> Nos actualités</button>
+        <br> <p class='p2'><i>ou</i></p>
+        <button class='blc'> Nous contacter</button>
+        </div>
+    </div>
+
+    <div class="trois">
+        <div class='imgLine'>
+            <img src="../images/grandir.jpg" alt="">
+            <div class='purple'></div>
+            <h1 >01</h1>
+            <h4>Autonomiser</h4>
+
+            <p>Lorem ipsums quam accusantium sint, lsd ki be alain aut, i so fall denna agend a ser vi som consequatur asperiores nihil hic necessitatibus quis similique fugiat veritatis nesciunt cumque vel itaque omnis! Nesciunt, iure numquam!</p>
+        </div>
+        <div class='imgLine'>
+        <img src="../images/renforcer.jpg" alt="">
+        <div class='beige'></div>
+        <h1 >02</h1>
+            <h4>Développer</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis ipsa a sit quos aliquid fugiat, dicta facilis voluptatem alias modi est dignissimos dolorem fugit ab voluptatum labore neque blanditiis aperiam?</p>
+        </div>
+        <div class='imgLine'>
+        <img src="../images/clint-adair-BW0vK-FA3eg-unsplash.jpg" alt="">
+        <div class='gris'></div>
+        <h1>03</h1>
+            <h4>Connecter</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione a veniam obcaecati quod earum? Non asperiores aliquid delectus cupiditate voluptate rem ad iure quis. Blanditiis aspernatur nemo alias quis corporis.</p>
+        </div>
+    </div>
+
+    <div class="pres">
+       <div class="presTop">
+      <div class="tex">
+      <h5 class='f1'>CJED rassemble</h5>
+        <h4>
+        Le Club est un espace de partage.
+        </h4>
+        <p>Le Club vous invite à partager votre expérience, discuter de vos problématiques en toute confiance et en toute liberté, travailler sur les sujets qui touchent votre quotidien de chef d’entreprise avec des experts, rencontrer de futurs partenaires-clients-fournisseurs, développer un réseau ….</p>
+    
+      </div>
+      
+    <img src="../images/v.jpg" alt="">
+       </div>
+    <div class="etor">
+    <h3>Notre présence et notre portée </h3>
+    <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h5>  
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero laboriosam nostrum officiis asperiores inventore, eligendi ad dignissimos maxime aliquam quod praesentium quia quam ratione quos dolores nemo iusto fuga id!</p>
+    </div>
+   
+    <div class="boxProgress">
+   
+    <div class="boxen">
+    <h5>Djibouti</h5>
+    <div class="progress">
+      <div class="progress-bar " role="progressbar" style="width: 100%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"> 
+      <i class="material-icons">
+    check_circle
+    </i>
+    </div>
+    </div>
+    </div>
+
+    <div class="boxen">
+    <h5>Ali-Sabieh</h5>
+    <div class="progress">
+      <div class="progress-bar " role="progressbar" style="width: 100%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"> 
+      <i class="material-icons">
+    check_circle
+    </i>
+    </div>
+    </div>
+    </div>
+
+    <div class="boxen">
+    <h5>Tadjoura</h5>
+    <div class="progress">
+      <div class="progress-bar " role="progressbar" style="width: 100%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"> 
+      <i class="material-icons">
+    check_circle
+    </i>
+    </div>
+    </div>
+    </div>
+
+    <div class="boxen">
+    <h5>Dikhil</h5>
+    <div class="progress">
+      <div class="progress-bar " role="progressbar" style="width: 100%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"> 
+      <i class="material-icons">
+    check_circle
+    </i>
+    </div>
+    </div>
+    </div>
+
+    <div class="boxen">
+    <h5>Obock</h5>
+    <div class="progress">
+      <div class="progress-bar " role="progressbar" style="width: 100%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"> 
+      <i class="material-icons">
+    check_circle
+    </i>
+    </div>
+    </div>
+    </div>
+
+    <div class="boxen">
+    <h5>Arta</h5>
+    <div class="progress">
+      <div class="progress-bar " role="progressbar" style="width: 100%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"> 
+      <i class="material-icons">
+    check_circle
+    </i>
+    </div>
+    </div>
+    </div>
+
+
+    </div>
+       </div>
+
+    <div class='mission'>
+        <h1>la CJED </h1>
+       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt enim beatae distinctio molestias cupiditate itaque, odio modi repellendus totam labore, rem unde. Porro eos assumenda quibusdam deserunt, magni architecto iusto.</p>
+        <div class="goal">
+            <h3>Mission</h3>
+            <p>Notre mission est de connecter, inspirer et supporter les entrepreneurs pour 
+                atteindre le succès à travers notre communauté. <br>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat consequatur est ea cum provident corrupti. Fuga nihil natus atque ea reiciendis, error, veritatis doloribus magnam minus ab sunt, perspiciatis sapiente.
+            </p>
+        </div>
+
+        <div class="vision">
+        <h3>Vision </h3>
+            <p>Notre cible est de devenir le leader dans le secteur de l'entreprise chez les jeunes.
+                <br> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus quod voluptatem, repudiandae laboriosam architecto similique ut unde illo quos! Voluptates vel soluta commodi inventore odit blanditiis ipsum numquam accusantium officiis?
+            </p>
+        </div>
+
+        <div class="valeur">
+        <h3>Valeur </h3>
+            <p>La CJED a pour devise : responsabilité, compétitivité, intégrité, communauté.
+                <br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut perferendis, quia quisquam, illo veritatis rerum maiores nobis inventore magni et dolorem itaque assumenda possimus quaerat distinctio! Hic officiis illo incidunt! </p>
+        </div>
+    <button class='btnMore'> En savoir plus ... </button>
+    </div>
+
+
+
+    <div class='members'>
+    
+    <div class='membPic'>
+            <div class="imgBox">
+            <img src="../images/m2g.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                <h5>Mohamed Guirreh Galab</h5>
+                <p>Président du Club </p>
+
+                <div class="icons">
+                <a href="#"><img src="https://img.icons8.com/ios-filled/50/000000/facebook-new.png">
+            </a>
+            <a href="#">  <img src="https://img.icons8.com/android/48/000000/twitter.png">
+            </a>
+            <a href="#"> <img src="https://img.icons8.com/ios-filled/50/000000/linkedin-circled.png">
+            </a>
+            </div>
+                </div>
+            </div>
+    </div>
+
+    <div class='membPic'>
+        <div class="imgBox">
+        <img src="../images/ll.jpg" alt="">
+        </div>
+        <div class="details">
+              <div class="content">
+              <h5>Mohamed Guirreh Galab</h5>
+              <p>Vice-président du Club </p>
+
+              <div class="icons">
+              <a href="#"><img src="https://img.icons8.com/ios-filled/50/000000/facebook-new.png">
+        </a>
+     
+     
+        <a href="#">  <img src="https://img.icons8.com/android/48/000000/twitter.png">
+  </a>
+    <a href="#"> <img src="https://img.icons8.com/ios-filled/50/000000/linkedin-circled.png">
+  </a>
+</div>
+              </div>
+          </div>
+    </div>
+
+    <div class='membPic'>
+        <div class="imgBox">
+        <img src="../images/jh.jpg" alt="">
+        </div>
+        <div class="details">
+              <div class="content">
+              <h5>Mohamed Guirreh Galab</h5>
+              <p>Sécretaire générale du Club </p>
+
+              <div class="icons">
+              <a href="#"><img src="https://img.icons8.com/ios-filled/50/000000/facebook-new.png">
+        </a>
+     
+     
+        <a href="#">  <img src="https://img.icons8.com/android/48/000000/twitter.png">
+  </a>
+    <a href="#"> <img src="https://img.icons8.com/ios-filled/50/000000/linkedin-circled.png">
+  </a>
+</div>
+              </div>
+          </div>
+    </div>
+
+    <button> Rejoinniez-nous  </button>
+    </div>
+   
+    </div>
+
+    <footer>
+        <?php include('../php/footer.php'); ?>
+    </footer>
+</body>
+</html>
