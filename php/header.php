cc03204eb2ff9e6aaaf8d670a5c765d174e1ee09
chakal05 <chakirrasta@gmail.com>
2019-10-05 00:59:13 +0200

    header + index.php started

---
+++ php/header.php
@@ -0,0 +1,72 @@
+<?php
+
+echo "
+
+<div class='container-fluid ''>
+<nav class='navbar is-fixed-top ' role='navigation' aria-label='main navigation'>
+  <div class='navbar-brand'>
+    <a class='navbar-item' href='index.php'>
+      <img src='../images/cjedIcon.jpg' />
+    </a>
+
+    <a
+      role='button'
+      class='navbar-burger burger'
+      aria-label='menu'
+      aria-expanded='false'
+      data-target='navbarBasicExample'
+    >
+      <span aria-hidden='true'></span>
+      <span aria-hidden='true'></span>
+      <span aria-hidden='true'></span>
+    </a>
+  </div>
+
+  <div id='navbarBasicExample' class='navbar-menu'>
+    <div class='navbar-end'>
+      <div class='navbar-item has-dropdown is-hoverable'>
+        <a class='navbar-link'>
+          Le club des Entrepreneurs
+        </a>
+
+        <div class='navbar-dropdown  is-boxed'>
+          <a class='navbar-item'>
+            Qui sommes-nous
+          </a>
+          <a class='navbar-item'>
+            Le bureau
+          </a>
+          <a class='navbar-item'>
+            Nos actions
+          </a>
+          <a class='navbar-item'>
+            Nos agenda
+          </a>
+        </div>
+      </div>
+
+      <a class='navbar-item'>
+        Nos membres
+      </a>
+
+      <a class='navbar-item'>
+        Adhérer au Club
+      </a>
+
+      <a class='navbar-item'>
+        L'Actu du CLub
+      </a>
+
+      <a class='navbar-item'>
+        Nous contacter
+      </a>
+    </div>
+  </div>
+</nav>
+</div>
+
+";
+
+?>
+
+   
